8ec39149851c11fbb0a9dcf4c82aad5cf6bc9581
Bhavye Mathur <bhavyemathur@gmail.com>
2023-02-06 12:34:19 +0530

    Added a fit_krochanek_bartels_spline method()

--- wind-prediction/maths.py
+++ wind-prediction/maths.py
@@ -856,68 +856,68 @@ def rmse(data, prediction):
 """Interpolation Functions"""
 
 
-def linear_interpolate(values, index: int, mu: float):
-    return values[index] * (1 - mu) + values[index + 1] * mu
+def linear_interpolate(values, index: int, t: float):
+    return values[index] * (1 - t) + values[index + 1] * t
 
 
-def cosine_interpolate(values, index: int, mu: float):
-    return linear_interpolate(values, index, (1 - math.cos(mu * math.pi)) / 2)
+def cosine_interpolate(values, index: int, t: float):
+    return linear_interpolate(values, index, (1 - math.cos(t * math.pi)) / 2)
 
 
-def cubic_interpolate(values, index: int, mu: float):
+def cubic_interpolate(values, index: int, t: float):
     p0 = values[max(0, index - 1)]
     p1 = values[index]
     p2 = values[index + 1]
     p3 = values[min(len(values) - 1, index + 2)]
 
-    mu2 = mu * mu
+    t2 = t * t
     a0 = p3 - p2 - p0 + p1
     a1 = p0 - p1 - a0
     a2 = p2 - p0
     a3 = p1
 
-    return a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3
+    return a0 * t * t2 + a1 * t2 + a2 * t + a3
 
 
-def catmull_rom_interpolate(values, index: int, mu: float):
+def catmull_rom_interpolate(values, index: int, t: float):
     p0 = values[max(0, index - 1)]
     p1 = values[index]
     p2 = values[index + 1]
     p3 = values[min(len(values) - 1, index + 2)]
     
-    mu2 = mu * mu
+    t2 = t * t
 
     a0 = -0.5 * p0 + 1.5 * p1 - 1.5 * p2 + 0.5 * p3
     a1 = p0 - 2.5 * p1 + 2 * p2 - 0.5 * p3
     a2 = -0.5 * p0 + 0.5 * p2
     a3 = p1
 
-    return a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3
+    return a0 * t * t2 + a1 * t2 + a2 * t + a3
 
 
-def hermite_interpolate(values, index: int, mu: float, tension: float = 0, bias: float = 0):
+def hermite_interpolate(values, index: int, t: float, tension: float = 0, bias: float = 0):
     p0 = values[max(0, index - 1)]
     p1 = values[index]
     p2 = values[index + 1]
     p3 = values[min(len(values) - 1, index + 2)]
     
-    mu2 = mu * mu
-    mu3 = mu2 * mu
+    t2 = t * t
+    t3 = t2 * t
 
-    m0 = (p1 - p0) * (1 + bias) * (1 - tension)
-    m0 += (p2 - p1) * (1 - bias) * (1 - tension)
+    p2_minus_p1 = p2 - p1
 
-    m1 = (p2 - p1) * (1 + bias) * (1 - tension)
-    m1 += (p3 - p2) * (1 - bias) * (1 - tension)
-    m0 /= 2
-    m1 /= 2
+    m0 = (p1 - p0) * (1 + bias)
+    m0 += p2_minus_p1 * (1 - bias)
 
-    a0 = 2 * mu3 - 3 * mu2 + 1
-    a1 = mu3 - 2 * mu2 + mu
-    a2 = mu3 - mu2
-    a3 = -2 * mu3 + 3 * mu2
+    m1 = p2_minus_p1 * (1 + bias)
+    m1 += (p3 - p2) * (1 - bias)
 
-    return a0 * p1 + a1 * m0 + a2 * m1 + a3 * p2
+    a0 = 2 * t3 - 3 * t2 + 1
+    a1 = t3 - 2 * t2 + t
+    a2 = t3 - t2
+    a3 = -2 * t3 + 3 * t2
+
+    return a0 * p1 + (1 - tension) * (a1 * m0 + a2 * m1) / 2 + a3 * p2
 
 
 def get_hermite_interpolate(tension: float = 0, bias: float = 0):
@@ -925,3 +925,49 @@ def get_hermite_interpolate(tension: float = 0, bias: float = 0):
         return hermite_interpolate(*args, tension=tension, bias=bias)
 
     return _interpolate
+
+
+def fit_kochanek_bartels_spline(data, values, index: int, t: float, learning_rate: float = 0.5):
+    p0 = values[max(0, index - 1)]
+    p1 = values[index]
+    p2 = values[index + 1]
+    p3 = values[min(len(values) - 1, index + 2)]
+
+    t2 = t * t
+    t3 = t2 * t
+
+    best_loss = float("inf")
+    best_tension = 0
+    best_bias = 0
+
+    a0 = 2 * t3 - 3 * t2 + 1
+    a1 = (t3 - 2 * t2 + t) / 2
+    a2 = (t3 - t2) / 2
+    a3 = -2 * t3 + 3 * t2
+
+    const = a0 * p1 + a3 * p2
+    baseline = data - const
+
+    m00 = p1 - p0
+    m01 = p2 - p1
+    m10 = m01
+    m11 = p3 - p2
+
+    bias = 0
+    tension = 0
+
+    for _ in range(30):
+        m0 = m00 * (1 + bias) + m01 * (1 - bias)
+        m1 = m10 * (1 + bias) + m11 * (1 - bias)
+
+        if (loss := ((baseline - (a1 * m0 + a2 * m1) * (1 - tension)) ** 2).sum()) < best_loss:
+            best_loss = loss
+            best_tension = tension
+        else:
+            learning_rate /= 1.5
+
+        d_outer = 2 * (baseline - (a1 * m0 + a2 * m1) * (1 - tension))
+        dt = (d_outer * (a1 * m0 + a2 * m1)).mean()
+        tension -= learning_rate * dt
+
+    return best_tension, best_bias
