84893736cf3bad575824edb1135901d18ce0a6b8
Bhavye Mathur <bhavyemathur@gmail.com>
2024-02-13 21:33:01 +0530

    Added ARCO ERA-5 test notebooks

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ east_wind = MERRA2Dataset("data/wind/YAVG", "U", is_float16=True)
 constants_ds = "data/raw/MERRA2_101.const_2d_asm_Nx.00000000.nc4"
 fraction_land = MERRA2Dataset(constants_ds, "FRLAND")
 
-plot_dataset(east_wind, lev=35, nightshade=True,
-             time=[f"{day + 1}-{month + 1}" for month in range(12) for day in range(MONTH_DAYS[month])])
+plot_latitude_longitude_contour(east_wind, time="01-01", lev=35)
+
+# plot_dataset(east_wind, lev=35, nightshade=True,
+#              time=[f"{day + 1}-{month + 1}" for month in range(1) for day in range(MONTH_DAYS[month])])
