cd8c7741cf832ccd899a252fa7eda23e4f4b3c0d
Bhavye Mathur <bhavyemathur@gmail.com>
2024-02-14 15:09:27 +0530

    format_bytes() now automatically guesses the unit

--- modules/util.py
+++ modules/util.py
@@ -1,60 +1,36 @@
+from typing import Generator
+
+import datetime
 from time import strftime
+from math import log
+
 import string
 
 
-def format_bytes(size: int, unit, si: bool = False) -> str:
+def format_bytes(size: int, si: bool = False) -> str:
     """
-    Converts bytes to common units such as kb, kib, KB, mb, mib, MB
+    Converts bytes to printable units
 
     Parameters
     ---------
     size: int
         Number of bytes to be converted
 
-    unit: str
-        Desired unit of measure for output
-
     si: bool
         True -> Use SI standard e.g. KB = 1000 bytes
         False -> Use JEDEC standard e.g. KB = 1024 bytes
-
-    Returns
-    -------
-    str:
-        E.g. "7 MiB" where MiB is the original unit abbreviation supplied
     """
-    if unit.lower() in {"b", "bit", "bits"}:
-        return f"{size * 8} {unit}"
-    unit_name = unit[0].upper()+unit[1:].replace("s", "")  # Normalised
-    reference = {"Kb Kib Kibibit Kilobit": (7, 1),
-                 "KB KiB Kibibyte Kilobyte": (10, 1),
-                 "Mb Mib Mebibit Megabit": (17, 2),
-                 "MB MiB Mebibyte Megabyte": (20, 2),
-                 "Gb Gib Gibibit Gigabit": (27, 3),
-                 "GB GiB Gibibyte Gigabyte": (30, 3),
-                 "Tb Tib Tebibit Terabit": (37, 4),
-                 "TB TiB Tebibyte Terabyte": (40, 4),
-                 "Pb Pib Pebibit Petabit": (47, 5),
-                 "PB PiB Pebibyte Petabyte": (50, 5),
-                 "Eb Eib Exbibit Exabit": (57, 6),
-                 "EB EiB Exbibyte Exabyte": (60, 6),
-                 "Zb Zib Zebibit Zettabit": (67, 7),
-                 "ZB ZiB Zebibyte Zettabyte": (70, 7),
-                 "Yb Yib Yobibit Yottabit": (77, 8),
-                 "YB YiB Yobibyte Yottabyte": (80, 8),
-                 }
-    key_list = '\n'.join(["     b Bit"] + [x for x in reference.keys()]) + "\n"
-    if unit_name not in key_list:
-        raise IndexError(f"\n\nConversion unit must be one of:\n\n{key_list}")
-    units, divisors = [(k, v) for k, v in reference.items() if unit_name in k][0]
-    if si:
-        divisor = 1000**divisors[1]/8 if "bit" in units else 1000**divisors[1]
-    else:
-        divisor = float(1 << divisors[0])
-    value = size / divisor
-    return f"{value:,.0f} {unit_name}{(value != 1 and len(unit_name) > 3) * 's'}"
+
+    order = 0
+    while log(size) > 3:
+        size /= 1000 if si else 1024
+        order += 1
+
+    unit_name = size_units[order]
+    return f"{size:.2f} {unit_name if si else unit_name.replace('B', 'iB')}{size != 1 * 's'}"
 
 
+size_units = ("bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
 fmt = string.Formatter()
 
 
@@ -131,3 +107,24 @@ def parse_datetime(datetime: str, yavg: int = 0) -> tuple[int | None, int | None
 
     except Exception:
         raise ValueError(f"Invalid time format {datetime!r}")
+
+
+def datetime_range(start: str, end: str, delta: datetime.timedelta) -> Generator[datetime.datetime, None, None]:
+    minute1, hour1, day1, month1, year1 = parse_datetime(start)
+    minute2, hour2, day2, month2, year2 = parse_datetime(end)
+
+    start = datetime.datetime(1981 if year1 == 0 else year1,
+                              month1 if month1 else 1,
+                              day1 if day1 else 1,
+                              hour1 if hour1 else 1,
+                              minute1 if minute1 else 30)
+
+    end = datetime.datetime(1981 if year2 == 0 else year2,
+                            month2 if month2 else 12,
+                            day2 if day2 else 31,
+                            hour2 if hour2 else 22,
+                            minute2 if minute2 else 30)
+
+    while start <= end:
+        yield start
+        start += delta
