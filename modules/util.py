d217f1e93b607dd561a2e75b47775b3ef3e9e1c0
Bhavye Mathur <bhavyemathur@gmail.com>
2024-02-14 16:25:14 +0530

    Fixed plural print in util.format_bytes()

--- modules/util.py
+++ modules/util.py
@@ -27,7 +27,7 @@ def format_bytes(size: int, si: bool = False) -> str:
         order += 1
 
     unit_name = size_units[order]
-    return f"{size:.2f} {unit_name if si else unit_name.replace('B', 'iB')}{size != 1 * 's'}"
+    return f"{size:.2f} {unit_name if si else unit_name.replace('B', 'iB')}{'' if size == 1 else 's'}"
 
 
 size_units = ("bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
