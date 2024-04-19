0419e107d1773fe1bcbc2f1285db68756d4e796f
Bhavye Mathur <bhavyemathur@gmail.com>
2024-04-19 15:56:12 +0530

    Added era5_filename() function

--- modules/era5/arco_era5.py
+++ modules/era5/arco_era5.py
@@ -120,6 +120,19 @@ def compress_dataset(dataset: xr.Dataset, view: str = "int16", verbose: bool = T
     return dataset
 
 
+def era5_filename(datetime, is_tavg: bool = True) -> str:
+    file = ["ERA5"]
+    datetime = parse_datetime(datetime)
+
+    if is_tavg:
+        file.append("tavg")
+        file.append(format_datetime(datetime, pretty=False))
+    else:
+        raise NotImplementedError("Non-TAVG datasets unimplemented")
+
+    return "-".join(file) + ".nc"
+
+
 def save_dataset(dataset, output_folder: str, verbose: bool = True) -> None:
     """
     Saves the ERA-5 dataset
@@ -132,26 +145,17 @@ def save_dataset(dataset, output_folder: str, verbose: bool = True) -> None:
     Raises:
         NotImplementedError: dataset must be time-averaged
     """
-    file = ["ERA5"]
-
-    if dataset.attrs.get("is_tavg"):
-        file.append("tavg")
-
-        time = parse_datetime(dataset.attrs["datetime"])
-        file.append(format_datetime(time, pretty=False))
-    else:
-        raise NotImplementedError("Don't know how to save non-TAVG dataset")
-
+    
+    filepath = f"{output_folder}/{era5_filename(dataset.attrs['datetime'], dataset.attrs.get('is_tavg'))}"
     if verbose:
         with ProgressBar():
-            dataset.to_netcdf(f"{output_folder}/{'-'.join(file)}.nc")
+            dataset.to_netcdf(filepath)
     else:
-        dataset.to_netcdf(f"{output_folder}/{'-'.join(file)}.nc")
+        dataset.to_netcdf(filepath)
 
 
-@datetime_func("time")
 def era5_file_exists(time, output_folder: str = "ERA5/"):
     """
     Checks if a locally saved ERA-5 file exists
     """
-    return os.path.isfile(f"{output_folder}/ERA5-tavg-{format_datetime(time, pretty=False)}.nc")
+    return os.path.isfile(f"{output_folder}/{era5_filename(time)}")
